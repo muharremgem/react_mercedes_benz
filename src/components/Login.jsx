e0598aa0a839ebeabad94f0fb945b2a42861c3fd
muharrem gem <muharremgem28@gmail.com>
2022-10-29 16:54:40 +0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import merco from "../assets/login.jpg";
+import { SiMercedes } from "react-icons/si";
 
 const Login = () => {
   return (
     <div>
-      <div className="bg-black p-14 text-white text-xl"> Mercedes-Benz</div>
+      <div className="flex flex-col bg-black p-8 text-white text-xl">
+        <SiMercedes className="flex text-[4rem] " />
+        <p className="flex ml-[6rem] font-thin">Mercedes-Benz</p>
+      </div>
       <div>
-        <img className="w-full" src={merco} alt="" />
+        <img className="w-full" src={merco} alt="merco" />
       </div>
       <div className="bg-black"></div>
+      <p>muharrrem</p>
     </div>
   );
 };
