28c5f34dea851284878eb1ad97389e104624c6af
muharrem gem <muharremgem28@gmail.com>
2022-10-29 19:53:13 +0300

    Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <div className="flex items-end  bg-black p-8 text-white text-xl">
+      <div className="flex items-end bg-black p-8 text-white text-xl">
         <img className="w-[5rem] " src={icon} alt="icon" />
         <p className="flex mt-12 ml-[1rem] font-thin">Mercedes-Benz</p>
-        <div className="flex items-end">
+        <div className="flex mx-auto">
           <div className="flex gap-5 ">
             <NavLink
               className=" transition ease-in-out delay-100 hover:scale-125 text-white"
