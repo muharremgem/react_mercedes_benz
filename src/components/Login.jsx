7f92816c11a9d74bdff35da27baec8d1b1122528
muharrem gem <muharremgem28@gmail.com>
2022-10-29 18:09:05 +0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import merco from "../assets/login.jpg";
-import { SiMercedes } from "react-icons/si";
+import icon from "../assets/icon.jpeg";
 
 const Login = () => {
   return (
     <div>
-      <div className="flex flex-col bg-black p-8 text-white text-xl">
-        <SiMercedes className="flex text-[4rem] " />
-        <p className="flex ml-[6rem] font-thin">Mercedes-Benz</p>
-      </div>
-      <div>
-        <img className="w-full" src={merco} alt="merco" />
-      </div>
-      <div className="bg-black"></div>
-      <p>muharrrem</p>
+      <form className="flex flex-col ">
+        <div className="flex items-center bg-black p-8 text-white text-xl">
+          <img className="w-[5rem] " src={icon} alt="icon" />
+          <p className="flex mt-12 ml-[1rem] font-thin">Mercedes-Benz</p>
+        </div>
+        <div className="foto ">
+          <div className="flex  flex-col justify-center items-center rounded-full">
+            <input
+              className="bg-orange-500 m-5"
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className="bg-green-500"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
